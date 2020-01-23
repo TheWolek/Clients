7ff77772c656f2450b4dd6d740c1dcf98a4aba4e
TheWolek <pawelm916@wp.pl>
2020-01-23 16:00:17 +0100

    .sql fix

--- clients.sql
+++ clients.sql
@@ -1,2 +1,2 @@
-ALTER TABLE `clients` MODIFY `numer_farby` text COLLATE utf8_bin NOT NULL
-ALTER TABLE `clients_backup` MODIFY `numer_farby` text COLLATE utf8_bin NOT NULL
+ALTER TABLE `clients` MODIFY `numer_farby` text COLLATE utf8_bin NOT NULL;
+ALTER TABLE `clients_backup` MODIFY `numer_farby` text COLLATE utf8_bin NOT NULL;
