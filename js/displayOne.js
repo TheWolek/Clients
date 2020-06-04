3007b8ec3bdabe15a9a4dca47388d695ffb29c88
TheWolek <pawelm916@wp.pl>
2020-06-04 22:35:13 +0200

    span imie i nazwisko przy find

--- js/displayOne.js
+++ js/displayOne.js
@@ -15,19 +15,19 @@ function findOne(id) {
     inEditMode = false
     inDeleteMode = false
 
-    $.get(url,function(data) {
+    $.get(url, function (data) {
         //console.log(data)
         data = JSON.parse(data)
         //console.log(data)
         let CID = data.clientData.CID
         let name = data.clientData.dane
         let procs = data.procs
-        
+
         let output = "<div class='client-card' id='client-card'>"
-        output += "<span class='client-id' id='client-id'>"+CID+"</span> <span class='client-name' id='client-name"+CID+"'>Imię i Nazwisko: "+name+"</span><button class='btn btn-primary client-proc-add' onclick='addProc("+CID+")'>Dodaj</button><div id='client-proc'>Zabiegi:</div></div>"
+        output += "<span class='client-id' id='client-id'>" + CID + "</span> <span class='client-name' id='client-name" + CID + "'>Imię i Nazwisko: <span class='text-danger'>" + name + "</span></span><button class='btn btn-primary client-proc-add' onclick='addProc(" + CID + ")'>Dodaj</button><div id='client-proc'>Zabiegi:</div></div>"
         area.html(output)
-        for(let i=0;i<procs.length;i++) {
-            appendProc(procs[i].ID,procs[i].tresc)
+        for (let i = 0; i < procs.length; i++) {
+            appendProc(procs[i].ID, procs[i].tresc)
         }
         displayAddProcForm(CID)
     })
